ea3b974560c1be977785ad9a1b0b0e04178eb382
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-11-17 10:15:10 -0300

    Ajustando banco de dados

--- src/models/users.go
+++ src/models/users.go
@@ -1,1 +1,13 @@
 package models
+
+import "time"
+
+// User cria um usuário
+type User struct {
+	Id        uint      `json:"id,omitempty'`
+	Name      string    `json:"name,omitempty"`
+	Email     string    `json:"email,omitempty"`
+	Nick      string    `json:"nick,omitempty"`
+	Password  string    `json:"password,omitEmpty"`
+	CreatedAt time.Time `json:"CreatedAt,omitEmpty"`
+}

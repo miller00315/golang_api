8e8815f1f0feaedeba128e58e34902fe9753a334
Miller Cesar de Oliveira <miller00315@gmail.com>
2020-11-29 17:21:16 -0300

    Ajustando controller

--- src/controllers/publications.go
+++ src/controllers/publications.go
@@ -6,6 +6,7 @@ import (
 	"api/src/repositories"
 	"api/src/responses"
 	"encoding/json"
+	"errors"
 	"io/ioutil"
 	"net/http"
 	"strconv"
@@ -33,12 +34,12 @@ func CreatePublication(w http.ResponseWriter, r *http.Request) {
 
 	publication.AuthorID = userID
 
-	if error = publication.Prepare(); error != nil {
+	if error = json.Unmarshal(requestBody, &publication); error != nil {
 		responses.Error(w, http.StatusBadRequest, error)
 		return
 	}
 
-	if error = json.Unmarshal(requestBody, &publication); error != nil {
+	if error = publication.Prepare(); error != nil {
 		responses.Error(w, http.StatusBadRequest, error)
 		return
 	}
@@ -125,7 +126,64 @@ func GetPublication(w http.ResponseWriter, r *http.Request) {
 }
 
 // UpdatePublication
-func UpdatePublication(w http.ResponseWriter, r *http.Request) {}
+func UpdatePublication(w http.ResponseWriter, r *http.Request) {
+	userID, error := authentication.GetUserId(r)
+
+	if error != nil {
+		responses.Error(w, http.StatusUnauthorized, error)
+		return
+	}
+
+	params := mux.Vars(r) // Pega todos os parâmetros da rota
+
+	publicationID, error := strconv.ParseUint(params["publicationId"], 10, 64)
+
+	if error != nil {
+		responses.Error(w, http.StatusBadRequest, error)
+		return
+	}
+
+	db, error := SetDatabase(w)
+
+	if error != nil {
+		return
+	}
+
+	defer db.Close()
+
+	repository := repositories.NewPublicationRepository(db)
+
+	databasePublication, error := repository.GetPublication(publicationID)
+
+	if error != nil {
+		responses.Error(w, http.StatusInternalServerError, error)
+		return
+	}
+
+	if databasePublication.AuthorID != userID {
+		responses.Error(w, http.StatusForbidden, errors.New("Cant update publication of others users"))
+		return
+	}
+
+	requestBody, error := ioutil.ReadAll(r.Body)
+
+	if error != nil {
+		responses.Error(w, http.StatusBadRequest, error)
+		return
+	}
+
+	var publication models.Publication
+
+	if error = json.Unmarshal(requestBody, &publication); error != nil {
+		responses.Error(w, http.StatusBadRequest, error)
+		return
+	}
+
+	if error = publication.Prepare(); error != nil {
+		responses.Error(w, http.StatusBadRequest, error)
+		return
+	}
+}
 
 // DeletePublication
 func DeletePublication(w http.ResponseWriter, r *http.Request) {}
